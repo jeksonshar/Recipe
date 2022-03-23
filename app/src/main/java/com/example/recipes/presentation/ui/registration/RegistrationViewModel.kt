ea46055de5275919ad6642322b2e0d2ba0299305
JeksonShar <dev.srx981@gmail.com>
2022-03-23 22:59:14 +0200

    удалены изменения для теста

--- app/src/main/java/com/example/recipes/presentation/ui/registration/RegistrationViewModel.kt
+++ app/src/main/java/com/example/recipes/presentation/ui/registration/RegistrationViewModel.kt
@@ -14,8 +14,6 @@ import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.userProfileChangeRequest
 import java.lang.Exception
 
-//test remote удалить
-//test2 remote удалить
 class RegistrationViewModel : ViewModel() {
 
     private val _signUpOrLogIn = MutableLiveData(LOG_IN)
